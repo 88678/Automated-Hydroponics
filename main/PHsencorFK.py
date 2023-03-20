9428e2997a2586728db3c666777ef8fc1128dbaf
88678 <jonsoncc7@gmail.com>
2023-03-20 12:06:41 +0800

    0

---
+++ main/PHsencorFK.py
@@ -0,0 +1,9 @@
+import time
+
+ph = 7
+        
+        
+for i in range(5):
+    
+    print(ph)
+    time.sleep(1)
--- sensor program/21開關.py
+++ sensor program/21開關.py
@@ -3,7 +3,7 @@ import time
 
 pinRelay = 21#繼電器訊號腳
 #pinBn = 7 #微動開關接腳
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)#将GPIO模式设置为BCM，这是指定树莓派GPIO引脚的两种模式之一（另一种是BOARD模式）。
 GPIO.setup(pinRelay, GPIO.OUT) #gpio21輸出
 #GPIO.setup(pinBn, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
