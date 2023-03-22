599988dab55ff2055102fa4dcbaea19e8944f998
88678 <jonsoncc7@gmail.com>
2023-03-22 09:10:19 +0800

    sql基本ok

--- sql/updataSQL.py
+++ sql/updataSQL.py
@@ -1,4 +1,8 @@
 import mysql.connector
+import time
+
+# 格式化成2016-03-20 11:45:39形式
+time = (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
 ph=7
 
@@ -7,6 +11,10 @@ connection = mysql.connector.connect(host = 'localhost',port = '3306',user = 'ro
 cursor = connection.cursor()        #告訴他要開始使用了?#cursor光標
 
 sql = "INSERT INTO hydroponics (time, ph) VALUES (%s, %s);"
-new_data = (null, ph)
-cursor = connection.cursor()
-cursor.execute(sql, new_data)
+new_data = (time, ph)
+
+cursor.execute(sql, new_data)   #???
+
+connection.commit() #有動到資料 都要寫這個 才會提交指令
+
+connection.close()  #關閉connection連線
